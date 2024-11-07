50b065701958c227a827b837082c9a117f051ab7
kmdripon1991 <kmdripon1991@gmail.com>
2024-11-07 15:29:24 +0600

    problem-8 solution  added

--- src/problem-8.ts
+++ src/problem-8.ts
@@ -1,1 +1,6 @@
 // Create a function validateKeys(obj: T, keys: (keyof T)[]) that takes an object obj and an array of keys keys. The function should return true if all of the specified keys exist in the object; otherwise, it should return false.
+
+function validateKeys<T extends object>(obj: T, keys: (keyof T)[]): boolean {
+  const isExist: boolean = keys.every((key) => key in obj);
+  return isExist;
+}
